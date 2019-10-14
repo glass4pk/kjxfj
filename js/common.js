92ec54bdb5ba62dbaf47f97ff4d7d6542d7e0590
glass4pk <glass4pk@gmail.com>
2019-10-14 18:01:49 +0800

    第一版

---
+++ js/common.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Yimiky on 2019/10/11.
+ */
+
+
+$(document).ready(function(){
+  //检测屏幕高度
+  winW = document.documentElement.clientWidth || document.body.clientWidth;
+  winH = document.documentElement.clientHeight || document.body.clientHeight;
+
+
+  $(".page").css({
+    // minHeight:(winW*1334/750) + "px"
+    minHeight:winH + "px" ,//
+	left:"-"+(1920-winW)/2+"px",
+  });
+  
+  
+
+});
